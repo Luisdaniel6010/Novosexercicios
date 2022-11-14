b9fdc6ebd05a78d293c26fc880f32b4b070196c9
Luisdaniel6010 <6010ldaniel@gmail.com>
2022-11-14 07:57:47 -0300

    ex10b.html

--- exerciciosaulajs/exjason.js
+++ exerciciosaulajs/exjason.js
@@ -1,6 +1,51 @@
 b1.style.color='white';
 b1.style.backgroundColor= 'blue';
 
-const 
+    const pegarDadosCep = () => {
+    let dados = 0;
+    let ldados = [];
+    const cep = document.getElementById('cep').value
+   
+
+    const url = "https://viacep.com.br/ws";
+
+     fetch(`${url}/${cep}/json/`)
+    .then(response => response.json())
+    .then (dados => {
+        console.log("dados", dados)
+        
+    })
+}
+
+function newdados() {
+    let ldados = document.getElementById('nlista');
+   let dados = "";
+    const url = "https://viacep.com.br/ws";
+
+    fetch(`${url}/${cep}/json/`)
+   .then(response => response.json())
+   .then(ldados => ("ldados", ldados))
+    
+    let nelist = document.createElement('li');
+    nelist.textContent = ldados;
+    
+    ldados.appendChild(nelist);
+    }
+
+
+
+
+
+
+/*fetch("https://pokeapi.co/api/v2/pokemon?offset=0&limit=10")
+.then(Response => Response.json())
+.then(pokemon => setDatall(pokemon)){
+
+    document.appendChild('nlista');
+
+}*/
+
+
+
 
 
